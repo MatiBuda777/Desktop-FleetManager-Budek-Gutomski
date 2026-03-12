5459e018fed06ebfe1091a78c58993eee9628e18
mateuszbudek <mateusz.budek@cyfroweszkoly.pl>
2026-03-12 10:35:09 +0100

    create class Vehicle

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -2,5 +2,8 @@ namespace FleetManager.Models;
 
 public class Vehicle
 {
-    
+    private string Name;
+    private string RegistrationNumber;
+    private float Fuel;
+    private VehicleStatus Status;
 }
