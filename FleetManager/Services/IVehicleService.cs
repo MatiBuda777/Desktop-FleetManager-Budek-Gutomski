caf2c72b52fcf0d8226a28990215bbdd97d7cd7e
szymon509 <szymon.gutomski@cyfroweszkoly.pl>
2026-03-19 10:52:26 +0100

    Interfejs, asynchroniczne pobieranie listy pojazdów oraz zapisywanie

--- FleetManager/Services/IVehicleService.cs
+++ FleetManager/Services/IVehicleService.cs
@@ -1,6 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FleetManager.Models;
+
 namespace FleetManager.Services;
 
 public interface IVehicleService
 {
+    Task<IEnumerable<Vehicle>> LoadVehiclesAsync();
     
+    Task SaveVehiclesAsync(IEnumerable<Vehicle> vehicles);
 }
