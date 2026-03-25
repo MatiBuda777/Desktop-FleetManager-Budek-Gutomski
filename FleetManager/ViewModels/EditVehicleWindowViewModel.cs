c22b5518401f445949a3cf57674f9f41c07cbb55
mateuszbudek <mateusz.budek@cyfroweszkoly.pl>
2026-03-25 12:32:04 +0100

    Create Edit Window

---
+++ FleetManager/ViewModels/EditVehicleWindowViewModel.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using FleetManager.Models;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace FleetManager.ViewModels;
+
+public class EditVehicleWindowViewModel : ViewModelBase
+{
+    [Reactive] private string NewName { get; set; }
+    [Reactive] private string NewRegistrationNumber { get; set; }
+    [Reactive] private float NewFuel { get; set; }
+    [Reactive] private VehicleStatus NewStatus { get; set; }
+    
+    public VehicleStatus[] VehicleStatuses { get; } = Enum.GetValues<VehicleStatus>();
+    
+    public ReactiveCommand<Unit, Unit> SaveCommand { get; }
+
+    public EditVehicleWindowViewModel(Vehicle? vehicle)
+    {
+        SaveCommand = ReactiveCommand.Create(SaveChanges);
+        if (vehicle == null) return;
+        
+        NewName = vehicle.Name;
+        NewRegistrationNumber = vehicle.RegistrationNumber;
+        NewFuel = vehicle.Fuel;
+        NewStatus = vehicle.Status;
+    }
+
+    private void SaveChanges()
+    {
+        if (new List<string> { NewName, NewRegistrationNumber }.Any(string.IsNullOrWhiteSpace))
+        {
+            Console.WriteLine("!----------------! Nazwa i rejestracja pojazdu nie może być pusta!!! !-----------------!");
+            return;
+        }
+        
+        
+    }
+}
--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Reactive;
 using System.Reactive.Linq;
+using System.Text.Json;
 using FleetManager.Models;
+using ReactiveUI;
 
 namespace FleetManager.ViewModels;
 
@@ -11,14 +14,50 @@ public class MainWindowViewModel : ViewModelBase
 {
     public ObservableCollection<Vehicle> Vehicles { get; set; } = [];
     private const string FilePath = "Assets/Vehicles.json";
+    
+    public ReactiveCommand<Unit, Unit> AddVehiclesCommand { get; } // to implement
+    public ReactiveCommand<Unit, Unit> SaveVehiclesCommand { get; }
 
     public MainWindowViewModel()
     {
-        loadVehicles();
+        LoadVehicles();
+        AddVehiclesCommand = ReactiveCommand.Create(EditVehicleWindowViewModel);
+        SaveVehiclesCommand = ReactiveCommand.Create(SaveVehiclesToJson);
     }
 
-    private void loadVehicles()
+    private void LoadVehicles()
     {
         if (!File.Exists(FilePath)) return;
+
+        try
+        {
+            var jsonData = File.ReadAllText(FilePath);
+            var list = JsonSerializer.Deserialize<List<Vehicle>>(jsonData);
+            Vehicles.Clear();
+
+            if (list == null) return;
+            foreach (var vehicle in list)
+            {
+                Vehicles.Add(vehicle);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private void SaveVehiclesToJson()
+    {
+        try
+        {
+            var jsonData = JsonSerializer.Serialize(Vehicles);
+            File.WriteAllText(FilePath, jsonData);
+            Console.WriteLine("Vehicles saved to {0}", FilePath);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
