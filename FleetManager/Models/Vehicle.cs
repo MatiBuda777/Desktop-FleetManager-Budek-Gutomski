279923d050caa4d19f231ba949431d06f8578d90
szymon509 <szymon.gutomski@cyfroweszkoly.pl>
2026-04-28 14:05:58 +0200

    klasa Vehicle.cs i VehicleItemViewModel.cs

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -1,9 +1,34 @@
+using ReactiveUI;
+
 namespace FleetManager.Models;
 
-public class Vehicle
+public class Vehicle : ReactiveObject
 {
-    private string Name;
-    private string RegistrationNumber;
-    private float Fuel;
-    private VehicleStatus Status;
+    private string _name = string.Empty;
+    public string Name 
+    { 
+        get => _name; 
+        set => this.RaiseAndSetIfChanged(ref _name, value); 
+    }
+
+    private string _registrationNumber = string.Empty;
+    public string RegistrationNumber 
+    { 
+        get => _registrationNumber; 
+        set => this.RaiseAndSetIfChanged(ref _registrationNumber, value); 
+    }
+
+    private float _fuel;
+    public float Fuel 
+    { 
+        get => _fuel; 
+        set => this.RaiseAndSetIfChanged(ref _fuel, value); 
+    }
+
+    private VehicleStatus _status;
+    public VehicleStatus Status 
+    { 
+        get => _status; 
+        set => this.RaiseAndSetIfChanged(ref _status, value); 
+    }
 }
--- FleetManager/ViewModels/VehicleItemViewModel.cs
+++ FleetManager/ViewModels/VehicleItemViewModel.cs
@@ -1,7 +1,55 @@
-// (ViewModel dla UserControl)
+using System.Reactive;
+using ReactiveUI;
+using FleetManager.Models;
+
 namespace FleetManager.ViewModels;
 
 public class VehicleItemViewModel : ViewModelBase
 {
-    
+    public Vehicle Vehicle { get; }
+
+    // Komendy
+    public ReactiveCommand<Unit, Unit> RefuelCommand { get; }
+    public ReactiveCommand<Unit, Unit> DispatchCommand { get; }
+    public ReactiveCommand<Unit, Unit> ServiceCommand { get; }
+    public ReactiveCommand<Unit, Unit> MakeAvailableCommand { get; }
+
+    public VehicleItemViewModel(Vehicle vehicle)
+    {
+        Vehicle = vehicle;
+
+        // Walidacja: Blokada tankowania, gdy pojazd jest InRoute
+        var canRefuel = this.WhenAnyValue(
+            x => x.Vehicle.Status,
+            status => status != VehicleStatus.InRoute
+        );
+
+        // Walidacja: Blokada wysyłki w trasę, gdy paliwo < 15% lub status to Service
+        var canDispatch = this.WhenAnyValue(
+            x => x.Vehicle.Status,
+            x => x.Vehicle.Fuel,
+            (status, fuel) => status != VehicleStatus.Service && fuel >= 15.0f
+        );
+
+        // Inicjalizacja komend
+        RefuelCommand = ReactiveCommand.Create(() => 
+        { 
+            Vehicle.Fuel = 100f; 
+        }, canRefuel);
+
+        DispatchCommand = ReactiveCommand.Create(() => 
+        { 
+            Vehicle.Status = VehicleStatus.InRoute; 
+        }, canDispatch);
+
+        ServiceCommand = ReactiveCommand.Create(() => 
+        { 
+            Vehicle.Status = VehicleStatus.Service; 
+        });
+
+        MakeAvailableCommand = ReactiveCommand.Create(() => 
+        { 
+            Vehicle.Status = VehicleStatus.Available; 
+        });
+    }
 }
