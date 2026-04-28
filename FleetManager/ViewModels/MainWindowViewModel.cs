271c1b9367016bc6ccd17f8e1cf25c02595c2cdd
szymon509 <szymon.gutomski@cyfroweszkoly.pl>
2026-04-28 14:08:30 +0200

    główne okno MainWindowViewModel.cs

--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,45 @@
-﻿namespace FleetManager.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using FleetManager.Services;
+using FleetManager.Models;
+
+namespace FleetManager.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Witamy";
+    private readonly IVehicleService _vehicleService;
+    
+    public ObservableCollection<VehicleItemViewModel> Vehicles { get; } = new();
+
+    // Konstruktor bezparametrowy dla Designera (Avalonia)
+    public MainWindowViewModel() 
+    {
+        _vehicleService = new JsonVehicleService();
+        _ = LoadVehiclesAsync();
+    }
+
+    public MainWindowViewModel(IVehicleService vehicleService)
+    {
+        _vehicleService = vehicleService ?? throw new ArgumentNullException(nameof(vehicleService));
+        _ = LoadVehiclesAsync();
+    }
+
+    private async Task LoadVehiclesAsync()
+    {
+        try
+        {
+            var vehicles = await _vehicleService.LoadVehiclesAsync();
+            Vehicles.Clear();
+            
+            foreach (var vehicle in vehicles)
+            {
+                Vehicles.Add(new VehicleItemViewModel(vehicle));
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Błąd ładowania: {ex.Message}");
+        }
+    }
 }
